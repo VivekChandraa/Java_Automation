8cfb1b7c0a41ff005b05f5f44edf222a070241f2
VivekChandra <vivek.chandra@upgrad.com>
2025-11-13 22:23:05 +0530

    Initial commit

---
+++ src/Test/lab1.java
@@ -0,0 +1,13 @@
+package Test;
+
+public class lab1 {
+    public static void main(String[] args) {
+        int age=1;
+        if (age >18){
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("Nop");
+        }
+    }
+}
